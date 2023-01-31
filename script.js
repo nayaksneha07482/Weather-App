d1bb149ad1553c2ec2b07d16c3b616554b0da349
nayaksneha07482 <nayaksneha07482@gmail.com>
2023-01-31 13:14:47 +0530

    seven day fore castjs

--- script.js
+++ script.js
@@ -47,3 +47,61 @@ document.querySelector("#btn").addEventListener("click", () => {
         document.querySelector("#speed").textContent = data.wind.speed + " kmph";
     }
 });
+
+
+// Seven day forecast
+
+
+document.querySelector("#seven").addEventListener("click", () => {
+    document.querySelector("#seven").style.borderBottom = "3px solid rgb(96, 89, 114)";
+    document.querySelector("#today").style.borderBottom = "none";
+    document.querySelector("#weather").style.display = "none";
+    document.querySelector("#map").style.display = "none";
+
+    var city = document.querySelector("#inp").value;
+
+    async function getWeeklyWeather() {
+        try {
+            let key = "9889d1c207146349f199645bad7f13c6";
+            let fetched = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=7&appid=${key}&units=metric`);
+
+            let data = await fetched.json();
+            let dataArr = data.list;
+            console.log(dataArr);
+            weekly(dataArr);
+        }
+        catch (err) {
+            console.log('err:', err);
+        }
+    }
+    getWeeklyWeather();
+
+    function weekly(dataArr) {
+        dataArr.map(item => {
+            let cardDiv = document.createElement("div");
+            cardDiv.setAttribute("id", "cards")
+
+            let date = document.createElement("p");
+            date.textContent = item.dt_txt.split(" ").shift();
+
+            let imgDiv = document.createElement("div");
+            let image = document.createElement("img");
+            image.src = `http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`;
+
+            let tempDiv = document.createElement("div");
+            let minTemp = document.createElement("p");
+            minTemp.textContent = "Min: " + item.main.temp_min + "°";
+
+            let maxTemp = document.createElement("p");
+            maxTemp.textContent = "Max: " + item.main.temp_max + "°";
+
+            let clouds = document.createElement("p");
+            clouds.textContent = item.weather[0].description;
+            clouds.setAttribute("id", "clouds")
+            imgDiv.append(image);
+            tempDiv.append(minTemp, maxTemp);
+            cardDiv.append(date, imgDiv, tempDiv, clouds);
+            document.querySelector("#container").append(cardDiv);
+        });
+    }
+});
